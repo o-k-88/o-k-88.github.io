a83563d086462b86da0149bf892d378f806fa79c
oleh_kyrylenko <okgoalkeeperschool@gmail.com>
2022-08-29 01:22:23 +0300

    ADAPTIVE

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 window.onscroll = function () { myFunction() };
 
 // Get the navbar
-var navbar = document.getElementById("nav");
+var nav = document.getElementById("nav");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = nav.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     if (window.pageYOffset > sticky) {
-        navbar.classList.add("sticky")
+        nav.classList.add("sticky")
     } else {
-        navbar.classList.remove("sticky");
+        nav.classList.remove("sticky");
     }
 }
 
