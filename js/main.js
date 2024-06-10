7654e49898962decf2a39c9d5380a85b85a5f621
Oleh Kyrylenko <oleh.kyrylenko@esc.edu>
2024-06-10 10:11:40 -0400

    Swiper added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
 const scrollTopBtn = document.querySelector("#scrollTop");
 
 function scrollTop() {
